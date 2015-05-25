d295a3bd108032d3dbf2d191444142f4d201a83f
Patrick Worthey <pworthey007@msn.com>
2015-05-24 21:03:56 -0700

    Adding enemy and player

--- SomethingAwesome/SomethingAwesome/Main.cpp
+++ SomethingAwesome/SomethingAwesome/Main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include "Enemy.h"
+#include "Player.h"
 
 int main(){
-	std::cout << "Whatever you want" << std::endl;
+
+	Enemy Jaafar;
+	Jaafar.Boats = 12;
+	Jaafar.Books = 0;
+	Jaafar.CreatureType = 2;
+	Jaafar.HitPoints = 9001;
+	Jaafar.Introduction = "My name is Endigo Jaafar. You killed my father, prepare to die!";
+	Jaafar.IsFemale = false;
+	Jaafar.MoustacheType = 9;
+	Jaafar.IsHipster = true;
+	Jaafar.Volume = 100;
+	Jaafar.WeaponType = 16;
+
+	std::cout << Jaafar.Introduction << std::endl;
+	std::cout << "Number of boats: " << Jaafar.Boats << std::endl;
+	std::cout << Jaafar.IsFemale << std::endl;
+	Player Player1;
+	std::cin >> Player1.Boats;
+	std::cin >> Player1.Books;
+	std::cin >> Player1.CreatureType;
+	Player1.HitPoints = 9002;
+	std::cin >> Player1.IsFemale;
+	std::cin >> Player1.MoustacheType;
+	std::cin >> Player1.IsHipster;
+	std::cin >> Player1.Volume;
+	std::cin >> Player1.WeaponType;
+
+
 	return 0;
 }
---
+++ SomethingAwesome/SomethingAwesome/Player.h
@@ -0,0 +1,94 @@
+#pragma once
+
+#include <string>
+
+class Player
+{
+public:
+
+	/*
+	Beard = 0
+	Chin curtain
+	Chin strap beard
+	Clean shaven
+	Friendly muttonchops
+	Fu Manchu = 5
+	Goat patch
+	Goatee
+	German Goatee
+	Handlebar moustache
+	Horseshoe = 10
+	Neckbeard
+	Pencil
+	muttonchops
+	sidewhiskers
+	shenandoah = 15
+	sideburns
+	soul patch
+	stubble
+	toothbrush
+	van dyke = 20
+	walrus
+	*/
+	int MoustacheType;
+
+	/*
+	Bare hands = 0
+	flamethrower
+	car bomb
+	catapult
+	sword
+	crossbow = 5
+	dagger
+	grenade
+	garrote wire
+	m60 machine gun
+	pistol whip = 10
+	scissors
+	scorpion
+	slingshot
+	vampire fangs
+	zombie bite = 15
+	hot coffee
+	*/
+	int WeaponType;
+
+	/*
+	Human = 0
+	zombie
+	dragon
+	unicorn
+	liger
+	angel = 5
+	basilisk
+	bugbears
+	demon
+	dinosaur
+	dwarf = 10
+	elf
+	worm
+	gargoyle
+	ghost
+	gnome = 15
+	hag
+	halfling
+	hydra
+	mummy
+	orc = 20
+	skeleton
+	troll
+	worg
+	shark
+	*/
+	int CreatureType;
+	int Volume;
+	int Books;
+	int Boats;
+	int HitPoints;
+	bool IsFemale;
+	bool IsHipster;
+
+	Player();
+	~Player();
+};
+
